66a684d888395151ec8a5b6e5e08c767c7c54774
Xiang Zhang <xz95@uw.edu>
2021-01-07 16:06:40 -0800

    can return to home page after click save btn

--- Android_Development/SimpleToDo/app/src/main/java/edu/neu/madcourse/simpletodo/EditActivity.java
+++ Android_Development/SimpleToDo/app/src/main/java/edu/neu/madcourse/simpletodo/EditActivity.java
@@ -1,7 +1,9 @@
 package edu.neu.madcourse.simpletodo;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
@@ -19,5 +21,21 @@ public class EditActivity extends AppCompatActivity {
         btnSave = findViewById(R.id.btnSave);
 
         getSupportActionBar().setTitle("Edit item");
+
+        etItem.setText(getIntent().getStringExtra(MainActivity.KEY_TASK_TEXT));
+        btnSave.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // create an intent which will contain the results
+                Intent intent = new Intent();
+                // pass the data (results of editing)
+                intent.putExtra(MainActivity.KEY_TASK_TEXT, etItem.getText().toString());
+                intent.putExtra(MainActivity.KEY_TASK_POSITION, getIntent().getExtras().getInt(MainActivity.KEY_TASK_POSITION));
+                // set the result of the intent
+                setResult(RESULT_OK, intent);
+                // finish activity, close the screen and go back
+                finish();
+            }
+        });
     }
 }
