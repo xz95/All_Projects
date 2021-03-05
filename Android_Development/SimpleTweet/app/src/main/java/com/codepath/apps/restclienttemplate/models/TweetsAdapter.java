77b6043aaddd2d4cd905acf51051a57ce26443a4
Xiang Zhang <xz95@uw.edu>
2021-03-05 11:46:31 -0800

    add tweets in recycler view

--- Android_Development/SimpleTweet/app/src/main/java/com/codepath/apps/restclienttemplate/models/TweetsAdapter.java
+++ Android_Development/SimpleTweet/app/src/main/java/com/codepath/apps/restclienttemplate/models/TweetsAdapter.java
@@ -1,4 +1,76 @@
 package com.codepath.apps.restclienttemplate.models;
 
-public class TweetsAdapter {
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.bumptech.glide.Glide;
+import com.codepath.apps.restclienttemplate.R;
+
+import java.util.List;
+
+public class TweetsAdapter extends RecyclerView.Adapter<TweetsAdapter.ViewHolder> {
+
+    Context context;
+    List<Tweet> tweets;
+    // pass in the context and list of tweets
+    public TweetsAdapter(Context context, List<Tweet> tweets) {
+        this.context = context;
+        this.tweets = tweets;
+    }
+
+    // for each row, inflate the layout for tweet
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(context).inflate(R.layout.item_tweet, parent, false);
+        return new ViewHolder(view);
+    }
+
+    // Bind values based on the position of the element
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+        // Get the data at position
+        Tweet tweet = tweets.get(position);
+        // Bind the tweet with View Holder
+        holder.bind(tweet);
+    }
+
+    @Override
+    public int getItemCount() {
+        return tweets.size();
+    }
+    // pass in the context and list of tweets
+
+
+
+    // Bind values based on the position of the element
+
+    // Define a viewHolder
+        public class ViewHolder extends RecyclerView.ViewHolder {
+
+            ImageView ivProfileImage;
+            TextView tvBody;
+            TextView tvScreenName;
+
+        public ViewHolder(@NonNull View itemView) {
+            super(itemView);
+            ivProfileImage = itemView.findViewById(R.id.ivProfileImage);
+            tvBody = itemView.findViewById(R.id.tvBody);
+            tvScreenName = itemView.findViewById(R.id.tvScreenName);
+        }
+
+        public void bind(Tweet tweet) {
+            tvBody.setText(tweet.body);
+            tvScreenName.setText(tweet.user.screenName);
+            Glide.with(context).load(tweet.user.profileImageUrl).into(ivProfileImage);
+        }
+    }
+
 }
